ac48dfc70ec33afbe06ae65d51d16a138dbc5fac
Katia Celestino <celestinokp@gmail.com>
2024-07-14 19:19:26 -0400

    fixed librerias sin usar

--- src/com/alurachallenge/monedasconversor/principal/Principal.java
+++ src/com/alurachallenge/monedasconversor/principal/Principal.java
@@ -1,13 +1,5 @@
 package com.alurachallenge.monedasconversor.principal;
-
-import com.alurachallenge.monedasconversor.modelos.ConsultaAPI;
-import com.alurachallenge.monedasconversor.modelos.DatosValorMonedas;
 import com.alurachallenge.monedasconversor.modelos.Menu;
-import com.google.gson.Gson;
-import com.google.gson.JsonParser;
-
-import java.util.Objects;
-import java.util.Scanner;
 
 public class Principal {
 
